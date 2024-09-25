6b4ee03d3fd32adc18ed1b360b6f93ea49d4d9c0
Twoeyes22 <btwoeyes@gmail.com>
2024-09-25 16:31:06 +0900

    ㅎㅎ 함수도 test_가 들어가야 한대!

--- project/240903/test_09_03_project1.py
+++ project/240903/test_09_03_project1.py
@@ -1,1 +1,9 @@
 print("헤헤 에라 모르겠다 print만 해보자")
+
+def test_dummy():
+    print("이것은 더미 테스트 함수입니다.")
+    assert True  # 항상 통과하는 간단한 테스트
+
+def test_another_dummy():
+    print("이것은 또 다른 더미 테스트 함수입니다.")
+    assert 1 + 1 == 2  # 간단한 산술 테스트
